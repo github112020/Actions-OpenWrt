84b6cd84d59d34f53768eeff2f1e59e0a3d0bd00
github112020 <65796925+github112020@users.noreply.github.com>
2020-05-23 10:15:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+
+# 魔改 SSRP+
+git clone https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/helloworld
+
+# 刷新&安装源
+./scripts/feeds update -a
+./scripts/feeds install -a
